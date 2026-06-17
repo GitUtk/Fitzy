34526ed7d7768fb7d9fb2876e21e57172bd1ffda
Utkarsh <84007458+GitUtk@users.noreply.github.com>
2026-06-17 18:18:02 +0530

    Add CORS middleware to FastAPI application
    Added CORS middleware to allow all origins, methods, and headers.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.api.v1.router import router as v1_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Fitzy Backend APIs"
     )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #####
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(v1_router, prefix="/api/v1")
